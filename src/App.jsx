8d88549c8a59c9a1beded29c57c753d6781d79cb
thomas <thomas.zeryouh@goons.fr>
2025-06-24 17:13:36 +0200

    act 11

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 import "./App.css";
 import { Button, Card, Container, Form } from "react-bootstrap";
 import { useForm } from "react-hook-form";
@@ -22,6 +23,39 @@ function App() {
     console.log(data);
     reset();
   };
+  const schema = yup.object().shape({
+    name: yup
+      .string()
+      .min(5, "doit comporter au moins 3 caractères")
+      .max(15, "doit comporter maximum 15 caractères")
+      .required("le nom est requis"),
+    date: yup
+      .string()
+      .matches(
+        /^((0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4})$/,
+        "La date est invalide"
+      )
+      .transform((value) => {
+        const { d, m, y } = value.split("/");
+        return new Date(`${y}-${m}-${d}`);
+      })
+      .test(
+        "test1",
+        "la date doit être aujourd'hui ou plus tard",
+        function (value) {
+          const now = new Date();
+          return value >= now;
+        }
+      ),
+    priority: yup
+      .string()
+      .oneOf(
+        ["Basse", "Moyenne", "Haute"],
+        "La priorité doit être « Basse », « Moyenne » ou « Haute »"
+      ),
+    isCompleted: yup.boolean(),
+  });
+
   return (
     <Container>
       <Card className="bg-blue">
@@ -36,18 +70,18 @@ function App() {
               })}
               placeholder="Entrer le nom de la tache"
             />
-            {errors.name && <p>{errors.name.message}</p>}
+            <p>{errors.name?.message}</p>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Date</Form.Label>
             <Form.Control
-              type="date"
+              type="text"
               {...register("date", {
                 required: "La date de la tâche est requise",
               })}
             />
-            {errors.date && <p>{errors.date.message}</p>}
+            <p>{errors.date?.message}</p>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Label>Priorité</Form.Label>
@@ -59,6 +93,7 @@ function App() {
               <option value="Moyenne">Moyenne</option>
               <option value="Haute">Haute</option>
             </Form.Select>
+            <p>{errors.priority?.message}</p>
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicCheckbox">
             <Form.Check
